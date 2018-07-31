9acd5b1edaf844830ef72ed83dd33283520f9d0c
Dylan <dylan314@optonline.net>
2018-07-31 01:08:04 -0400

    the thing

---
+++ mods/ra/maps/deadnight/deadnight.lua
@@ -0,0 +1,10 @@
+
+SendWave = function()
+
+end
+
+WorldLoaded = function()
+	Player1 = Player.GetPlayer("Spain")
+	Player2 = Player.GetPlayer("Germany")
+	Zomboids = Player.GetPlayer("BadGuy")
+end
