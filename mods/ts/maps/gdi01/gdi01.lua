933b1a21f0c5b5424cfcee32a26151ede52074b7
Dylan Manitta <dylan@plugnpay.com>
2018-08-07 16:07:14 -0400

    Fixes

--- mods/ts/maps/gdi01/gdi01.lua
+++ mods/ts/maps/gdi01/gdi01.lua
@@ -1,12 +1,46 @@
 BaseIsBuilt = false
+SendWaves = true
 ticks = 0
+NodInfAttack = { };
+ValidForces = { 'e1', 'e3', 'cyborg', 'nahand', 'napower', 'buggy' }
 
 SendReinforcements = function()
 	Reinforcements.ReinforceWithTransport(GDI, 'dshp', {'e1','e1','e1','e1','e1','e2','e2'}, {CPos.New(44,10), Landing.Location},{Landing.Location, CPos.New(44,10)})
 end
 
 SendEnemies = function()
-	Reinforcements.ReinforceWithTransport(Nod, 'sapc', {'e1', 'e1', 'e1', 'e1'}, {CPos.New(44,1), CPos.New(50,17)}, {CPos.New(44,1)})
+	Reinforcements.ReinforceWithTransport(Nod, 'sapc', {'cyborg', 'e1', 'e1', 'e1'}, {CPos.New(44,1), CPos.New(50,17)}, {CPos.New(44,1)})
+end
+
+BuildInfantry = function()
+	if NodHand.IsDead or not SendWaves then
+		return
+	end
+
+	local delay = Utils.RandomInteger(DateTime.Seconds(3), DateTime.Seconds(9))
+	local toBuild = { 'e1','e1','e1','e3','e3' }
+	Nod.Build(toBuild, function(unit)
+		NodInfAttack[#NodInfAttack + 1] = unit[1]
+		if #NodInfAttack >= 5 then
+			SendUnits(NodInfAttack, {CPos.New(44,1)})
+			NodInfAttack = { }
+			--Trigger.AfterDelay(DateTime.Minutes(2), BuildInfantry)
+		else
+			--Trigger.AfterDelay(delay, BuildInfantry)
+		end
+	end)
+end
+
+SendUnits = function(units, waypoints)
+	Utils.Do(units, function(unit)
+		if not unit.IsDead then
+			Utils.Do(waypoints, function(waypoint)
+				unit.AttackMove(waypoint.Location)
+			end)
+			Trigger.OnIdle(unit, function() unit.Hunt() end)
+		end
+	end)
+	SendWaves = false
 end
 
 InitObjectives = function()
@@ -26,7 +60,11 @@ CheckBaseRequirements = function()
 end
 
 CheckEnemyUnitsRemaining = function()
-	local unitCount = #Nod.GetActors()
+	local unitCount = 0
+	Utils.Do(validForces, function(actor) 
+		unitCount = unitCount + #Nod.GetActorsByType(actor)
+	end)
+
 	if unitCount == 0 then
 		GDI.MarkCompletedObjective(KillEnemy)
 	end
@@ -36,6 +74,9 @@ WorldLoaded = function()
 	GDI = Player.GetPlayer("GDI")
 	Nod = Player.GetPlayer("Nod")
 	InitObjectives()
+	if SendWaves then
+		BuildInfantry()
+	end
 end
 
 Tick = function()
