4f657d9443f713892219752770fc07ea634f0cdb
Dylan Manitta <dylan@plugnpay.com>
2018-08-17 15:09:21 -0400

    Added first wave

--- mods/ts/maps/gdi02/gdi02.lua
+++ mods/ts/maps/gdi02/gdi02.lua
@@ -1,5 +1,10 @@
+InitObjective = function()
+
+end
+
 WorldLoaded = function()
 	GDI = Player.GetPlayer("GDI")
 	Nod = Player.GetPlayer("Nod")
 	Creeps = Player.GetPlayer("Creeps")
+	Reinforcements.Reinforce(GDI, { "mcv", "smech", "smech", "e1", "e1", "e1" }, { Actor1.Location, Actor2.Location })
 end
