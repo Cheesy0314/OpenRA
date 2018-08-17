be43726abba488f416548e69f4a2db0402b72841
Dylan Manitta <dylan@plugnpay.com>
2018-08-17 15:17:30 -0400

    Added obj

--- mods/ts/maps/gdi02/gdi02.lua
+++ mods/ts/maps/gdi02/gdi02.lua
@@ -6,5 +6,17 @@ WorldLoaded = function()
 	GDI = Player.GetPlayer("GDI")
 	Nod = Player.GetPlayer("Nod")
 	Creeps = Player.GetPlayer("Creeps")
-	Reinforcements.Reinforce(GDI, { "mcv", "smech", "smech", "e1", "e1", "e1" }, { Actor1.Location, Actor2.Location })
+        local transports = Reinforcements.Reinforce(GDI, { "orcatran", "orcatran" }, { Actor370.Location, Actor367.Location }, function(actor)
+		Trigger.OnDamaged(actor, function(self, attacker)
+			Trigger.AfterDelay(DateTime.Seconds(1), function() self.Kill() end)
+		end)
+	end)
+
+        Trigger.OnAllKilled(transports, function() 
+		SAMObjective = GDI.AddPrimaryObjective("Destroy SAM sites")
+		Trigger.AfterDelay(DateTime.Seconds(1), function()
+			Reinforcements.Reinforce(GDI, { "mcv", "smech", "smech", "e1", "e1", "e1" }, { Actor370.Location, CPos.New(61, 42) })
+		end)
+	end)
+       
 end
