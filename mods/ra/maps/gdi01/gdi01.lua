8f69b665f654c18b0516b32387088a0085fbbe33
Dylan <dylan314@optonline.net>
2018-08-21 21:03:16 -0400

    Added new map

---
+++ mods/ra/maps/gdi01/gdi01.lua
@@ -0,0 +1,13 @@
+WorldLoaded = function()
+	GDI = Player.GetPlayer("Spain")
+	Germany = Player.GetPlayer("Germany")
+	KillObj = GDI.AddPrimaryObjective("Destroy Enemy Forces")
+	Utils.Do(Germany.GetActorsByType("ca"), function(boat) 
+		boat.Attack(Actor274)
+		Trigger.OnIdle(boat, function() boat.Hunt() end)
+	end)
+	Trigger.OnKilled(Actor274, function() 
+		Reinforcements.ReinforceWithTransport(GDI, "lst", {"2tnk","2tnk","jeep","mcv","4tnk"}, {CPos.New(35, 128), Actor299.Location},{CPos.New(35, 128)})
+
+	end)
+end
