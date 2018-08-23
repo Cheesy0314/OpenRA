b2d24310e783d3ccb07a82324cabdc7d0fab56b7
Dylan Manitta <dylan@plugnpay.com>
2018-08-23 12:42:20 -0400

    Updated rules

--- mods/ra/maps/gdi01/gdi01.lua
+++ mods/ra/maps/gdi01/gdi01.lua
@@ -1,67 +1,77 @@
 ProducedUnitTypes =
-        {
-                { factory = SovietBarracks1, types = { "e1", "e4", "e4", "e1", "e1" }, delay = 3 },
-                { factory = SovietBarracks2, types = { "shok", "shok", "shok" }, delay = 5 },
-                { factory = SovietBarracks3, types = { "e1", "e3", "e3", "e1" }, delay = 1 },
-                { factory = SovietWarFactory1, types = { "jeep", "1tnk", "1tnk", "jeep" }, delay = 3 },
-                { factory = SovietWarFactory2, types = { "stnk", "stnk", "stnk" }, delay = 5 },
-                { factory = SovietHeliPad1, types = { "hind" }, delay = 4 },
-                { factory = SovietHeliPad2, types = { "hind" }, delay = 6}
-        }
+	{
+		{ factory = SovietBarracks1, types = { "e1", "e4", "e4", "e1", "e1" }, delay = 3 },
+		{ factory = SovietWarFactory1, types = { "jeep", "1tnk", "1tnk", "jeep" }, delay = 3 },
+		{ factory = SovietHeliPad2, types = { "hind" }, delay = 6}
+	}
 
 WorldLoaded = function()
 	GDI = Player.GetPlayer("Spain")
 	Germany = Player.GetPlayer("Germany")
 	KillObj = GDI.AddPrimaryObjective("Destroy Enemy Forces")
-	Utils.Do(Germany.GetActorsByType("ca"), function(boat) 
-		boat.Attack(Actor274)
-		Trigger.OnIdle(boat, function() boat.Hunt() end)
-	end)
+	Camera.Position = Actor274.CenterPosition
+	Trigger.AfterDelay(DateTime.Seconds(1), SendStrikeFighters)
 	Trigger.OnKilled(Actor274, function() 
-		Reinforcements.ReinforceWithTransport(GDI, "lst", {"2tnk","2tnk","jeep","mcv","4tnk"}, {CPos.New(35, 128), Actor299.Location - CVec.New(0,2)},{CPos.New(35, 128)})
+		Reinforcements.ReinforceWithTransport(GDI, "lst", {"2tnk","2tnk","jeep","mcv","4tnk"}, {CPos.New(35, 128), Actor302.Location},{CPos.New(35, 128)})
 		Trigger.AfterDelay(DateTime.Minutes(2), function()
 			TurnOnAI()
 		end)
 	end)
 end
 
-BindActorTriggers = function(a)
-        if a.HasProperty("Hunt") then
-                Trigger.OnIdle(a, function(a)
-                	if a.IsInWorld then
-                       		a.Hunt()
-			end
-        	end)
-        end
+SendStrikeFighters = function()
+	local fighter = Actor.Create("phant", true, {Owner = Coalition, Location =  CPos.New(35, 128)})
+       	fighter.Attack(Actor274)
+       	Trigger.OnKilled(Actor274, function()
+	       	fighter.Move(CPos.New(35, 128))
+	       	Trigger.AfterDelay(DateTime.Seconds(1), function() 
+			Trigger.OnIdle(fighter, function() 
+				fighter.Destroy() 
+			end) 
+		end)
+	end)
+end
 
-        if a.HasProperty("HasPassengers") then
-                Trigger.OnDamaged(a, function()
-                        if a.HasPassengers then
-                                a.Stop()
-                                a.UnloadPassengers()
-                        end
-                end)
-        end
+BindActorTriggers = function(a)
+	a.AttackMove(CPos.New(31,92))
+
+	if a.HasProperty("Hunt") then
+		Trigger.OnIdle(a, function(a)
+			if a.IsInWorld then
+		       		a.Hunt()
+			end
+		end)
+	end
+
+	if a.HasProperty("HasPassengers") then
+		Trigger.OnDamaged(a, function()
+			if a.HasPassengers then
+				a.Stop()
+				a.UnloadPassengers()
+			end
+		end)
+	end
 end
 
 TurnOnAI = function()
-        Utils.Do(ProducedUnitTypes, function(production)
-                Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
+	Utils.Do(ProducedUnitTypes, function(production)
+		Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
 		Trigger.AfterDelay(DateTime.Minutes(production.delay), function()
 			ProduceUnits(production)
 		end)
-        end)
+	end)
 end
 
 
 ProduceUnits = function(t)
-        local factory = t.factory
-        if not factory.IsDead then
+	local factory = t.factory
+	if not factory.IsDead then
 		Utils.Do(t.types, function(actorType) 
 			factory.Wait(Actor.BuildTime(actorType))
 			factory.Produce(actorType)
 			factory.Wait(5)
 		end)
+		
 		Trigger.AfterDelay(DateTime.Minutes(t.delay), function() ProduceUnits(t) end)
-        end
+	end
 end
