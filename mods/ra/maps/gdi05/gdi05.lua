3ed0007b76947d0cb4b2b223c5979d1e4e20b749
Dylan Manitta <dylan@plugnpay.com>
2018-08-24 08:20:30 -0400

    Added script/color

--- mods/ra/maps/gdi05/gdi05.lua
+++ mods/ra/maps/gdi05/gdi05.lua
@@ -1,7 +1,49 @@
 WorldLoaded = function()
 	GDI = Player.GetPlayer("Spain")
 	Iraq = Player.GetPlayer("Ukraine")
+	Civilians = Player.GetPlayer("Neutral")
+	AssassinateObj = GDI.AddPrimaryObjective("Assassinate Saddam Hussein")
+	UserInterface.SetMissionText("Assassinate Saddam", GDI.Color)
+	Reinforcements.Reinforce(GDI, { 'sniper' }, { CPos.New(23,32), CPos.New(23,29) }, 20, function(actor)
+		Trigger.OnKilled(actor, function()
+			GDI.MarkFailedObjective(AssassinateObj)
+		end)
+	end)
 
+	ActivateTriggers()
 end
 
+ActivateTriggers = function()
+	Trigger.OnPassengerEntered(EscapeAPC, function(apc,passenger)
+		if passenger == SaddamHussein then
+			EscapeAPC.Move(CPos.New(1,5))
+			Trigger.AfterDelay(25, function()
+				EscapeAPC.Destroy()
+				Trigger.AfterDelay(15, function() GDI.MarkCompletedObjective(AssassinateObj) end)
+			end)
+		end
+	end)
 
+	Trigger.OnDamaged(SaddamHussein, function(saddam, assassin)
+		saddam.EnterTransport(EscapeAPC)
+	end)
+end
+
+InitNotifications = function()
+	Trigger.OnObjectiveAdded(GDI, function(p, id)
+		Media.DisplayMessage(p.GetObjectiveDescription(id), "New " .. string.lower(p.GetObjectiveType(id)) .. " objective")
+	end)
+
+	Trigger.OnObjectiveCompleted(GDI, function(p, id)
+		Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective completed")
+	end)
+	Trigger.OnObjectiveFailed(GDI, function(p, id)
+		Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective failed")
+	end)
+ 	Trigger.OnPlayerLost(GDI, function()
+		Media.PlaySpeechNotification(GDI, "MissionFailed")
+	end)
+ 	Trigger.OnPlayerWon(GDI, function()
+		Trigger.AfterDelay(DateTime.Seconds(1), function() Media.PlaySpeechNotification(GDI, "MissionAccomplished")  end)
+	end)
+end
