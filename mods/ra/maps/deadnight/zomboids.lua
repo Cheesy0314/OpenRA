3c98b3d4ab3fb3dd16eff4199f891125c8210f68
Dylan Manitta <dylan@plugnpay.com>
2019-10-28 17:39:34 -0400

    Updating mission specs

--- mods/ra/maps/deadnight/zomboids.lua
+++ mods/ra/maps/deadnight/zomboids.lua
@@ -61,6 +61,13 @@ SendZombies = function()
 				
 			end)
 		end
+
+ 		if ticks == DateTime.Seconds(55) then
+			Media.DisplayMessage("Commander, mission parameters have changed... We are receiving odd reports from other landing sites. We are trying to make sense of the situation. Sit tight until we can figure out what is happening.", "Mission Command", Civilian.Color) 
+			Trigger.AfterDelay(1, function() Survive = Spain.AddPrimaryObjective("Defend beachhead") end)
+			Trigger.AfterDelay(3, function() Spain.MarkCompletedObjective(Defend) end)
+			
+		end
 	elseif (ticks > DateTime.Seconds(60) and ticks < DateTime.Seconds(121)) then
 		if (ticks == DateTime.Seconds(90) or ticks == DateTime.Seconds(120)) then
 				Reinforcements.Reinforce(Zombies, {"2tnk","jeep","1tnk","1tnk","jeep"}, { Actor196.Location, Actor205.Location}, 10, function(actor)
@@ -80,7 +87,7 @@ SendZombies = function()
 						actor.EnterTransport(LST)
 					end)
 					passengers = #actors
-					Media.DisplayMessage("WHAT THE HELL WERE THOSE?", "Team Leader", Germany.Color)
+					Media.DisplayMessage("WAS ZUM TEUFEL IST DAS?", "Feldwebel Müller", Germany.Color)
 					Media.PlaySoundNotification(Spain, "ChatLine")
 				end)
 			end
@@ -112,11 +119,16 @@ SendZombies = function()
 			end)
 		end
 
+		if (ticks == DateTime.Seconds(340)) then
+			Media.DisplayMessage("The tissue samples collected seem to confirm the disease theory, deploy medics to protect your troops Commander.", "Mission Command", Civilian.Color)
+			Media.PlaySoundNotification(Spain, "ChatLine")
+		end
+
 	elseif (ticks > DateTime.Seconds(360) and ticks < DateTime.Seconds(481)) then
 		if (ticks == DateTime.Seconds(362)) then
 		
 			Media.PlaySoundNotification(Spain, "ChatLine")
-			Media.DisplayMessage("!ATTENTION! We are airstriking local disease concentrations!","German Command",Germany.Color)
+			Media.DisplayMessage("! WARNUNG! Eingehende Luftangriffe zur Ausrottung infizierter Bevölkerungszentren!","Kommando Luftwaffe",Germany.Color)
 			Trigger.AfterDelay(DateTime.Seconds(5), function()
 				Media.PlaySpeechNotification(Spain, "AlliedReinforcementsArrived")
 				SendAirstrike(Actor198)
@@ -145,9 +157,9 @@ SendZombies = function()
 	elseif ticks > DateTime.Seconds(480) then
 		if (#Zombies.GetActorsByType("zombie")) == 0 and not MissionCompleted then
 			MissionCompleted = true
-			Media.DisplayMessage("Good job holding the fort Commander, we're analyzing the tissue samples recovered now.","CiviliaeMission Command", Civilian.Color)
+			Media.DisplayMessage("Good job holding the fort Commander, this area seems to be under control for now.","Mission Command", Civilian.Color)
 			Trigger.AfterDelay(DateTime.Seconds(10), function()
-				Spain.MarkCompletedObjective(Survive)
+				Spain.MarkCompletedObjective(Survive);
 			end)
 		end
 	end
@@ -170,13 +182,12 @@ GiveAirstrike = function()
 end
 
 NextObj = function()
-	Trigger.AfterDelay(1, function() Survive = Spain.AddPrimaryObjective("Fortify base and wait for further orders.") end)
+	Trigger.AfterDelay(1, function() Defend = Spain.AddPrimaryObjective("Fortify base and wait for further orders.") end)
 	Trigger.AfterDelay(8, function() Spain.MarkCompletedObjective(Beach) end)
-	Trigger.AfterDelay(50, function() Trigger.OnAllKilled(Spain.GetActorsByType('fact'), function() Spain.MarkFailedObjective(Survive) end) end)
+	Trigger.AfterDelay(50, function() Trigger.OnAllKilled(Spain.GetActorsByType('fact'), function() Spain.MarkFailedObjective(Defend) end) end)
 	Trigger.AfterDelay(50, function()
 		Media.PlaySoundNotification(Spain, "ChatLine")
-		Media.DisplayMessage("Goodwork Commander, We've been receiving odd messages from the enemy... It seems they've been coming into contact with an unknown force. Sit tight at your location and fortify while we try and figure this out.", "Mission Command", Civilian.Color)
-	
+		Media.DisplayMessage("Good work Commander the LZ is ours, build your base quickly and prepare for a counter attack.", "Mission Command", Civilian.Color)
 	end)
 	Trigger.AfterDelay(DateTime.Minutes(1), function() 
 		local reinf = Spain.GetActorsByType("lst")
