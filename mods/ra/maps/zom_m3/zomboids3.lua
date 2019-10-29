7ef2eb32e21b5a7067341e39a9336cd73a42be98
Dylan Manitta <dylan@plugnpay.com>
2019-10-29 13:45:37 -0400

    Update

--- mods/ra/maps/zom_m3/zomboids3.lua
+++ mods/ra/maps/zom_m3/zomboids3.lua
@@ -1,8 +1,40 @@
+InitialTeam = {}
+Reinforcements = {}
+ScienceTeam = {}
+AlliedForces = {}
+SovietForces = {}
+Infected = {}
+
+SendHorde = function()
+
+end
+
+FoundScienceTeam = function()
+
+end
+
+AlliesTurn = function()
+
+end
+
+Win = function()
+
+end
+
+Lose = function() 
+
+end
+
+InitNeeful = function()
+
+end
+
 WorldLoaded = function()
         Spain = Player.GetPlayer("Spain")
         Germany = Player.GetPlayer("Germany")
         Zombies = Player.GetPlayer("BadGuy")
         Civilian = Player.GetPlayer("Civilians")
+	SovietForces = Player.GetPlayer("Soviet")
         InitNeedful()
 
 end
